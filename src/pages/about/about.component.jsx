d78cd88ebca59dfc2cadfdb24f13ca60c7855136
rohannaam221b <your_email>
2021-08-09 11:50:01 +0530

    fourth

---
+++ src/pages/about/about.component.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./about.styles.css";
+import Profile from "../../assets/img/profile/rohannaam.webp";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+
+const About = () => {
+  return (
+    <div>
+      <div className="about">
+        <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
+        <Container>
+          <Row className="pt-3 pb-5 align-items-center">
+            <Col xs={12} md={6}>
+              <Row className="justify-content-center mr-2 mb-2">
+                <Image
+                  className="profile justify-content-end"
+                  alt="profile"
+                  src={Profile}
+                  thumbnail
+                  fluid
+                />
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row className=" align-items-start p-2 my-details rounded">
+                Hi there! I am <strong>&nbsp;Rohan J Rao</strong>
+                <br />A Engineer and a Learner, born and brought up
+                in India. I am a Junior Web Developer with React.js, Redux,
+                Express.js, Node.js and PostgreSQL as my tech stack.
+                <br />
+                In 2020, I successfully completed my Engineering with
+                specialization in 'Electronics and Telecommunication'.
+                <br />
+                Working with Websites, Adding new features, Playing with CSS and JavaScript is what i do.<br />
+                My Goal is to be an Expert in JavaScript.
+
+
+                <br />
+                Being a Self Taught Programmer I have encountered thousands of errors which ultimately taught me how to
+                Survive in this World of Programming.
+                <br />
+                <br /> <br />
+                <Col className="d-flex justify-content-center flex-wrap">
+                  <div>
+                    <a href="#contact">
+                      <Button className="m-2" variant="outline-primary">
+                        Let's talk
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://drive.google.com/file/d/1Snbq3x4Itk_ZN4c8J96aNdp4SunXR08Y/view?usp=sharing"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-success">
+                        My Resume
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://github.com/rohannaam221b"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-dark">
+                        GitHub
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a
+                      href="https://www.linkedin.com/in/rohan-rao-3a78b7188/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button className="m-2" variant="outline-info">
+                        LinkedIn
+                      </Button>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/skills/skills-data.js
@@ -0,0 +1,127 @@
+import L_HTML5 from "../../assets/img/skills/html-5.svg";
+import L_CSS3 from "../../assets/img/skills/css3.svg";
+
+import L_BOOTSTRAP from "../../assets/img/skills/bootstrap-4.svg";
+import L_REACT from "../../assets/img/skills/react.svg";
+import L_REDUX from "../../assets/img/skills/redux.svg";
+import L_REACT_ROUTER from "../../assets/img/skills/react-router.svg";
+
+import L_REACT_BOOTSTRAP from "../../assets/img/skills/react-bootstrap.svg";
+import L_STYLED_COMPONENTS from "../../assets/img/skills/styled-components.svg";
+import L_NODE_JS from "../../assets/img/skills/nodejs.svg";
+import L_EXPRESS from "../../assets/img/skills/express.svg";
+
+import L_POSTGRESQL from "../../assets/img/skills/postgresql.svg";
+
+
+
+import L_GITHUB_PAGES from "../../assets/img/skills/github.svg";
+import L_JAVASCRIPT from "../../assets/img/skills/javascript.svg";
+
+
+export const skills = {
+  frontend: [
+    {
+      link: "https://en.wikipedia.org/wiki/HTML5",
+      imgAltText: "HTML 5",
+      imgSrc: L_HTML5,
+      skillName: "HTML5",
+    },
+    {
+      link: "https://medium.com/beginners-guide-to-mobile-web-development/whats-new-in-css-3-dcd7fa6122e1",
+      imgAltText: "CSS 3",
+      imgSrc: L_CSS3,
+      skillName: "CSS3",
+    },
+    {
+      link: "https://www.javascript.com/",
+      imgAltText: "JavaScript",
+      imgSrc: L_JAVASCRIPT,
+      skillName: "JavaScript",
+    },
+
+    {
+      link: "https://getbootstrap.com/",
+      imgAltText: "Bootstrap",
+      imgSrc: L_BOOTSTRAP,
+      skillName: "Bootstrap",
+    },
+    {
+      link: "https://reactjs.org/",
+      imgAltText: "React JS",
+      imgSrc: L_REACT,
+      skillName: "React JS",
+    },
+    {
+      link: "https://redux.js.org/",
+      imgAltText: "Redux",
+      imgSrc: L_REDUX,
+      skillName: "Redux",
+    },
+    {
+      link: "https://reacttraining.com/react-router/",
+      imgAltText: "React Router",
+      imgSrc: L_REACT_ROUTER,
+      skillName: "React Router",
+    },
+
+    {
+      link: "https://react-bootstrap.github.io/",
+      imgAltText: "React Bootstrap",
+      imgSrc: L_REACT_BOOTSTRAP,
+      skillName: "React Bootstrap",
+    },
+    {
+      link: "https://styled-components.com/",
+      imgAltText: "styled-components",
+      imgSrc: L_STYLED_COMPONENTS,
+      skillName: "styled-components",
+    },
+  ],
+
+  backend: [
+    {
+      link: "https://nodejs.org/en/",
+      imgAltText: "Node.js",
+      imgSrc: L_NODE_JS,
+      skillName: "Node.js",
+    },
+    {
+      link: "https://expressjs.com/",
+      imgAltText: "Express",
+      imgSrc: L_EXPRESS,
+      skillName: "Express",
+    }
+
+  ],
+  hostingPlatforms: [
+
+
+    {
+      link: "https://pages.github.com/",
+      imgAltText: "GitHub Pages",
+      imgSrc: L_GITHUB_PAGES,
+      skillName: "GitHub Pages",
+    },
+  ],
+  programmingLanguages: [
+    {
+      link: "https://www.javascript.com/",
+      imgAltText: "JavaScript",
+      imgSrc: L_JAVASCRIPT,
+      skillName: "JavaScript",
+    }
+
+  ],
+  databases: [
+    {
+      link: "https://www.postgresql.org/",
+      imgAltText: "PostgreSQL",
+      imgSrc: L_POSTGRESQL,
+      skillName: "PostgreSQL",
+    }
+
+
+  ]
+
+};
