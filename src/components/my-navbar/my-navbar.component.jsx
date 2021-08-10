5e8b668f3c96dc2f639e9d40d4628bd646604170
rohannaam221b <83057789+rohannaam221b@users.noreply.github.com>
2021-08-10 08:52:44 +0530

    Update my-navbar.component.jsx

--- src/components/my-navbar/my-navbar.component.jsx
+++ src/components/my-navbar/my-navbar.component.jsx
@@ -52,8 +52,7 @@ const MyNavbar = () => {
             <Nav className="mr-auto ">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#about">About</Nav.Link>
-              <Nav.Link href="#skills">Skills</Nav.Link>
-              <Nav.Link href="#experience">Experience</Nav.Link>
+              <Nav.Link href="#skills">Skills</Nav.Link>            
               <Nav.Link href="#projects">Projects</Nav.Link>
               <Nav.Link href="#contact">Contact</Nav.Link>
             </Nav>
